154770d5870515d5a48087cb46374fa9b63d493c
Tom Beasley <tbeasley@healthequity.com>
2019-05-01 11:51:20 -0600

    Add project files.

---
+++ InterviewHolocron/Holocron.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace InterviewHolocron
+{
+    public class Holocron
+    {
+        public string ListCommands()
+        {
+
+            var programMembers = GetType().GetMembers();
+            var commands = Environment.NewLine;
+
+            foreach (MemberInfo info in programMembers)
+            {
+                if (info.MemberType == MemberTypes.Method && ((System.Reflection.MethodBase)info).IsVirtual == false && 
+                    (info.Name!="GetType"&&info.Name!="ListCommands"))
+                    commands += info.Name + Environment.NewLine;
+            }
+
+            if (commands.Length > 0)
+                return commands;
+            else
+                return "No accessible commands found!";
+
+
+        }
+
+        public string FizzBuzz(string InputNum)
+        {
+            int fbzr = 0;
+            if (!Int32.TryParse(InputNum, out fbzr))
+                return "I find your lack of number disturbing";
+            else
+                if (fbzr % 15 == 0)
+                return "Fizzbuzz";
+            else if (fbzr % 3 == 0)
+                return "Fizz";
+            else if (fbzr % 5 == 0)
+                return "Buzz";
+            else
+                return InputNum;
+                
+
+        }
+
+        public string ReverseString(string InputStr)
+        {
+
+            var originalString = InputStr;
+            var reversedString = "";
+
+            if (originalString.Length > 0 && !String.IsNullOrWhiteSpace(originalString))
+            {
+                for (int i = originalString.Length - 1; i > -1; i--)
+                {
+                    reversedString += originalString[i];
+                }
+                return reversedString;
+            }
+            else
+                return "No string to reverse!";
+
+        }
+    }
+}
---
+++ InterviewHolocron/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace InterviewHolocron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Holocron lightSaber = new Holocron();
+            var commandList = lightSaber.ListCommands();
+
+            Console.WriteLine("Please enter a command." + Environment.NewLine + "Available Commands are:" + lightSaber.ListCommands());
+
+            var InputSwitch = Console.ReadLine();
+
+            switch(InputSwitch)
+            {
+                case "ReverseString":
+                    {
+                        Console.WriteLine("Please supply a string to be reversed" + Environment.NewLine);
+                        Console.WriteLine(lightSaber.ReverseString());
+                        Console.ReadLine();
+                        break;
+                    }
+                case "FizzBuzz":
+                    {
+                        Console.WriteLine("Please supply a number to be FizzBuzzed" + Environment.NewLine);
+                        var InputStr = Console.ReadLine();
+                        Console.WriteLine(lightSaber.FizzBuzz(InputStr));
+                        Console.ReadLine();
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Sorry, that command is not recognized. Please Try Again.");
+                    break;
+            }
+        }
+
+        
+
+            
+
+
+
+        }
+    }
+
